bfb706a0ced0b8d76389b20217c52a0c7a031bdc
painted-pieces <graphics@paintedpiecesart.com>
2026-03-02 14:36:02 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'billboard-calculator-v1';
+const urlsToCache = [
+  '/billboard-calculator/',
+  '/billboard-calculator/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.8.2/jspdf.plugin.autotable.min.js'
+];
+
+// Install service worker and cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Caching app files');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch new
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        // If both cache and network fail, return offline page
+        return caches.match('/billboard-calculator/index.html');
+      })
+  );
+});
+
+// Update service worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
